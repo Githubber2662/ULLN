4d6df6653704a4bceaf79657624dee64897f896a
Githubber2662 <95174200+Githubber2662@users.noreply.github.com>
2026-04-19 11:47:20 +0300

    Updated numbers.js

--- numbers.js
+++ numbers.js
@@ -1,7 +1,7 @@
 const List1 = [["Zero", "0"], ["Reciprocal of Croutonplex (ill-defined)", "<sup>1</sup>&frasl;<sub>Croutonplex</sub>"], ["2 divided by Croutonplex (ill-defined)", "<sup>2</sup>&frasl;<sub>Croutonplex</sub>"], ["Croutonillionth (ill-defined)", "<sup>1</sup>&frasl;<sub>Croutonillion</sub>"],
                ["2 divided by Croutonillion (ill-defined)", "<sup>2</sup>&frasl;<sub>Croutonillion</sub>"]];
 var x = 0;
-for(x = 0; x < List1.length(); x++) {
+for(x = 0; x < List1.length; x++) {
    document.body.innerHTML += '<p id=`Number${x}`>'+List[x][0].toString()+', '+List[x][1].toString()+'</p>';
 }
 
